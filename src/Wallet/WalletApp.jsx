33806ec6da6405571b9ba9dae466231671b16458
sapjilmaster <sapjilmaster@gmail.com>
2023-12-16 20:42:49 +0900

    Feat file update

--- src/Wallet/WalletApp.jsx
+++ src/Wallet/WalletApp.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import WalletList from 'src/Wallet/WalletList';
 import WalletWrite from 'src/Wallet/WalletWrite';
 
 const WalletApp = () => {
-  // console.log('cc');
+  const [contents, setContents] = useState('list');
+
   return (
     <>
       <div>Today</div>
-      <WalletList />
-      <WalletWrite />
+      <div>
+        <button
+          onClick={() => {
+            setContents('list');
+          }}>
+          list
+        </button>
+        <button
+          onClick={() => {
+            setContents('write');
+          }}>
+          write
+        </button>
+      </div>
+      {contents === 'list' ? <WalletList /> : <WalletWrite />}
     </>
   );
 };
--- src/Wallet/WalletWrite.jsx
+++ src/Wallet/WalletWrite.jsx
@@ -1,5 +1,20 @@
 const WalletWrite = () => {
-  return <div>WalletWrite</div>;
+  return (
+    <>
+      <div>
+        <p>WalletWrite</p>
+        <div>
+          <input
+            type="text"
+            onChange={() => {
+              // setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <button onClick={() => {}}>write</button>
+      </div>
+    </>
+  );
 };
 
 export default WalletWrite;
