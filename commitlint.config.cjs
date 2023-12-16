1f53d89dad2b275586f5ad742242426218157414
sapjilmaster <sapjilmaster@gmail.com>
2023-12-16 22:12:21 +0900

    Feat: setting commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']],
+    'type-case': [2, 'always', ['pascal-case']],
+    'type-enum': [2, 'always', ['Feat', 'Fix', 'Docs', 'Style', 'Design', 'Refactor', 'Rename', 'Remove', 'Test']],
+  },
 };
